44a4d7daffde331d68bb2306101e3f24ed742431
Artem-Bld <baluevartem07@mail.ru>
2026-04-05 12:15:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,18 +14,18 @@ int countPairs2(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
     if (arr[i] > value) break;
-    int target = value - arr[i];
-    int left = i + 1;
-    int right = len - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (arr[mid] == target) {
+    for (int j = len - 1; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
         count++;
+        while (j > i && arr[j] == arr[j-1]) {
+          if (arr[i] + arr[j-1] == value) {
+            count++;
+          }
+          j--;
+        }
         break;
-      } else if (arr[mid] < target) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
+      } else if (arr[i] + arr[j] < value) {
+        break;
       }
     }
   }
@@ -38,34 +38,26 @@ int countPairs3(int *arr, int len, int value) {
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
-    int firstIndex = -1;
     while (left <= right) {
-      int mid = left + (right - left) / 2;
+      int mid = (left + right) / 2;
       if (arr[mid] == target) {
-        firstIndex = mid;
-        right = mid - 1;
+        count++;
+        int j = mid - 1;
+        while (j > i && arr[j] == target) {
+          count++;
+          j--;
+        }
+        j = mid + 1;
+        while (j < len && arr[j] == target) {
+          count++;
+          j++;
+        }
+        break;
       } else if (arr[mid] < target) {
         left = mid + 1;
       } else {
         right = mid - 1;
       }
-    }    
-    if (firstIndex != -1) {
-      left = firstIndex;
-      right = len - 1;
-      int lastIndex = firstIndex;
-      while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-          lastIndex = mid;
-          left = mid + 1;
-        } else if (arr[mid] < target) {
-          left = mid + 1;
-        } else {
-          right = mid - 1;
-        }
-      }
-      count += (lastIndex - firstIndex + 1);
     }
   }
   return count;
